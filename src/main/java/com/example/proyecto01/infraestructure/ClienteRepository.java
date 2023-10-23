9d62bcecf2c229379744150399d5af256dc70ba0
RODRIGO <rodrigo.meza@utec.edu.pe>
2023-10-23 15:34:26 -0500

    pe3

--- src/main/java/com/example/proyecto01/infraestructure/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.proyecto01.infraestructure;
-
-import com.example.proyecto01.domain.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente,Long> {
-}
